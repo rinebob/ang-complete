c101a455891ae9db84cf5b901b86f9dcd87d64c9
Bob Rinehart <rinebob111185@gmail.com>
2019-07-11 11:47:08 -0700

    lec 16-22.  Added alert and servers components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`
+    h3 {
+      color: red;
+    }
+  `],
+  // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'ang-complete';
+  name = 'Dude...';
 }
