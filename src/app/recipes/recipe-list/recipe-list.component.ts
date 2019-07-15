599eda286a2fc774b128f38e78c0db77881b233b
Robert Rinehart <rinebob111185@gmail.com>
2019-07-15 00:03:24 -0700

    through lec 86

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,8 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+	@Output() recipeWasSelected = new EventEmitter<Recipe>();
+
 	recipes: Recipe[] = [
 		new Recipe('A Test Recipe Dude', 'Hey it tastes really good!', 'https://coastguard.dodlive.mil/files/2014/11/photo-1.jpg'),
 
@@ -19,5 +21,9 @@ export class RecipeListComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  onRecipeSelected(recipe: Recipe) {
+	this.recipeWasSelected.emit(recipe);
+}
 
 }
