ce7c5bfbb05e6194d890ebe1c355d00ad14de4cd
Bob Rinehart <rinebob111185@gmail.com>
2019-08-09 09:25:17 -0700

    recipe service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,22 +7,22 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
-  private recipes: Recipe[] = [
+	private recipes: Recipe[] = [
 		new Recipe('A Test Recipe Dude', 'Hey it tastes really good!', 'https://coastguard.dodlive.mil/files/2014/11/photo-1.jpg', [
-      new Ingredient('bread', 2),
-      new Ingredient ('meat', 2)
-    ]),
+			new Ingredient('bread', 2),
+			new Ingredient ('meat', 2)
+	]),
 
-		new Recipe('Yo Hey Food Here', 'Man Im hungry dude!', 'https://coastguard.dodlive.mil/files/2014/11/photo-1.jpg', [
-      new Ingredient('hot dogs', 2),
-      new Ingredient ('buns', 2)
-    ]),
+	new Recipe('Yo Hey Food Here', 'Man Im hungry dude!', 'https://coastguard.dodlive.mil/files/2014/11/photo-1.jpg', [
+		new Ingredient('hot dogs', 2),
+		new Ingredient ('buns', 2)
+	]),
 
-		new Recipe('Like Test Recipe Three Dude', 'Oh not this same sht over again right?!', 'https://coastguard.dodlive.mil/files/2014/11/photo-1.jpg', [
-      new Ingredient('chips', 2),
-      new Ingredient ('salsa', 2)
-    ]),
-  ];
+	new Recipe('Like Test Recipe Three Dude', 'Oh not this same sht over again right?!', 'https://coastguard.dodlive.mil/files/2014/11/photo-1.jpg', [
+		new Ingredient('chips', 2),
+		new Ingredient ('salsa', 2)
+	]),
+	];
 
   constructor(private shoppingListService: ShoppingListService) {}
 
